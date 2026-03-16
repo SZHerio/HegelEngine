f6a550ec3b725d927e5d27a71f5b0e0ea6a5dffe
SZHerio <kir200207@gmail.com>
2026-03-16 18:52:55 +0300

    Initialized engine and sandbox startups

---
+++ engine/src/core/Application.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "engine/core/Application.h"
+
+namespace engine::core
+{
+    void Application::Run()
+    {
+        std::cout << "HegelEngine initializing...\n";
+    }
+}
---
+++ sandbox/src/main.cpp
@@ -0,0 +1,7 @@
+#include "engine/core/Application.h"
+
+int main()
+{
+    engine::core::Application app;
+    app.Run();
+}
